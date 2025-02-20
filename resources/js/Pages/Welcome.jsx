7c9d0281568eea243590887b3fd56d69f0b2599b
MehediHasanMridha <mehedimridhacse@gmail.com>
2025-02-20 15:39:22 +0600

    project setup

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,9 @@
+const Welcome = () => {
+    return (
+        <div className="flex flex-col h-screen w-full text-3xl font-bold">
+            Under construction
+        </div>
+    );
+};
+
+export default Welcome;
