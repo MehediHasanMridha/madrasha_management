28ce38c94ad652e9f1c2ec0bd755004f8d74eec6
MehediHasanMridha <mehedimridhacse@gmail.com>
2025-02-21 17:35:27 +0600

    add more content into sidebar

--- resources/js/Container/LeftSide/LeftSide.jsx
+++ resources/js/Container/LeftSide/LeftSide.jsx
@@ -1,9 +1,10 @@
+import CollapseIcon from "@/assets/images/collapse.svg";
+import Dashboard from "@/assets/images/dashboard.svg";
+import Finance from "@/assets/images/finance.svg";
 import Logo from "@/assets/images/logo.png";
 import SideBarUI from "@/Components/SideBarUI";
 import { Layout } from "antd";
 import { useState } from "react";
-
-import CollapseIcon from "@/assets/images/collapse.svg";
 
 const LeftSide = () => {
     const [collapsed, setCollapsed] = useState(false);
@@ -43,6 +44,50 @@ const LeftSide = () => {
                 </SideBarUI.Icon>
                 <SideBarUI.Text collapsed={collapsed}>Collapse</SideBarUI.Text>
             </SideBarUI.Item>
+            <div
+                className={`flex items-center space-x-[12px] my-2 ${
+                    collapsed ? "pl-0" : "pl-[24px]"
+                }`}
+            >
+                <SideBarUI.Text collapsed={collapsed}>
+                    Statistics
+                </SideBarUI.Text>
+                <hr className="bg-[#AFAFAF] h-[0.5px] w-full" />
+            </div>
+            <SideBarUI.Item
+                collapsed={collapsed}
+                className="flex h-[80px] items-center space-x-[12px] px-[50px]"
+            >
+                <SideBarUI.Icon>
+                    <img
+                        src={Dashboard}
+                        alt="Dashboard"
+                        className="w-[24px] h-[24px]"
+                    />
+                </SideBarUI.Icon>
+                <SideBarUI.Text collapsed={collapsed}>Dashboard</SideBarUI.Text>
+            </SideBarUI.Item>
+            <SideBarUI.Item
+                collapsed={collapsed}
+                className="flex h-[80px] items-center space-x-[12px] px-[50px]"
+            >
+                <SideBarUI.Icon>
+                    <img
+                        src={Finance}
+                        alt="Finance"
+                        className="w-[24px] h-[24px]"
+                    />
+                </SideBarUI.Icon>
+                <SideBarUI.Text collapsed={collapsed}>Finance</SideBarUI.Text>
+            </SideBarUI.Item>
+            <div
+                className={`flex items-center space-x-[12px] my-2 ${
+                    collapsed ? "pl-0" : "pl-[24px]"
+                }`}
+            >
+                <SideBarUI.Text collapsed={collapsed}>Campus</SideBarUI.Text>
+                <hr className="bg-[#AFAFAF] h-[0.5px] w-full" />
+            </div>
         </SideBarUI>
     );
 };
