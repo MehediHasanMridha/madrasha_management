6d898c43c01f4659c37643e793c6ed8bb090bf72
MehediHasanMridha <mehedimridhacse@gmail.com>
2025-02-27 21:54:14 +0600

    delete laravel 11 old file

--- resources/js/Components/LayoutUI.jsx
+++
@@ -1,8 +0,0 @@
-import { Layout } from "antd";
-
-const LayoutUI = ({ children }) => {
-    const { Content, Footer, Header, Sider } = Layout;
-    return <Layout>{children({ Content, Footer, Header, Sider })}</Layout>;
-};
-
-export default LayoutUI;
