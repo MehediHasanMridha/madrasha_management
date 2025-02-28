1e99470757506443f9fa4daf99d87e9f1bb118ad
MehediHasanMridha <mehedimridhacse@gmail.com>
2025-03-01 00:28:51 +0600

    bug fix

--- resources/js/pages/settings/appearance.jsx
+++ resources/js/pages/settings/appearance.jsx
@@ -1,11 +1,3 @@
-import { Head } from '@inertiajs/react';
-
-import AppearanceTabs from '@/components/appearance-tabs';
-import HeadingSmall from '@/components/heading-small';
-
-import AppLayout from '@/layouts/app-layout';
-import SettingsLayout from '@/layouts/settings/layout';
-
 const breadcrumbs = [
     {
         title: 'Appearance settings',
@@ -14,16 +6,5 @@ const breadcrumbs = [
 ];
 
 export default function Appearance() {
-    return (
-        <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Appearance settings" />
-
-            <SettingsLayout>
-                <div className="space-y-6">
-                    <HeadingSmall title="Appearance settings" description="Update your account's appearance settings" />
-                    <AppearanceTabs />
-                </div>
-            </SettingsLayout>
-        </AppLayout>
-    );
+    return <div className="">sfd</div>;
 }
