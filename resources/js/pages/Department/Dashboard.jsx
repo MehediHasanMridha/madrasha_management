ce38f3d64967a30f58ab72569dda44ea01b383d0
MehediHasanMridha <mehedimridhacse@gmail.com>
2025-03-27 02:21:29 +0600

    remove redundant

--- resources/js/pages/Department/Dashboard.jsx
+++ resources/js/pages/Department/Dashboard.jsx
@@ -1,4 +1,3 @@
-import LeftArrow from '@/assets/images/arrow-left.svg';
 import StudentSectionContainer from '@/Container/Department/StudentSectionContainer';
 import TeacherSectionContainer from '@/Container/Department/TeacherSectionContainer';
 import AuthenticatedLayout from '@/layouts/AuthenticatedLayout';
@@ -37,14 +36,6 @@ const Dashboard = ({ department, students, filters, sortOrder }) => {
     return (
         <AuthenticatedLayout>
             <Head title="Islamic School" />
-            <div className="inline-flex h-6 items-center justify-start gap-0.5">
-                <img src={LeftArrow} alt="arrow" className="h-[24px] w-[24px]" />
-                <div className="inline-flex space-x-0.5 text-base font-normal text-[#afafaf]">
-                    <span>Islamic school</span>
-                    <span>/</span>
-                    <span>Student</span>
-                </div>
-            </div>
             <div className="mt-[24px] flex space-x-[12px] rounded-[8px] bg-white p-[24px]">
                 <span
                     className={cn(
