049038510235c95f707d12dd8db39807baa78af7
MehediHasanMridha <mehedimridhacse@gmail.com>
2025-04-11 00:56:16 +0600

    refactor: Remove unused department and staff bound stores

--- resources/js/stores/useDepartmentBoundStore.js
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand';
-import { createStudentSlice } from './slices/studentSlice';
-
-export const useDepartmentBoundStore = create((...a) => ({
-    ...createStudentSlice(...a),
-}));
--- resources/js/stores/useStaffBoundStore.js
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand';
-import { createStaffSlice } from './slices/staffSlice';
-
-export const useStaffBoundStore = create((...a) => ({
-    ...createStaffSlice(...a),
-}));
