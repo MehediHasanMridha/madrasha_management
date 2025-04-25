135870f784d3dc8a9404afeb54899aafdaf727bf
MehediHasanMridha <mehedimridhacse@gmail.com>
2025-04-26 01:11:56 +0600

    feat: Refactor SearchComponent styling for improved readability and maintainability

--- resources/js/Components/Shared/SearchComponent.jsx
+++ resources/js/Components/Shared/SearchComponent.jsx
@@ -1,4 +1,5 @@
 import Field from '@/Components/UI/Field';
+import { cn } from '@/lib/utils';
 import { Search, X } from 'lucide-react';
 
 const SearchComponent = ({ showBtn, setShowBtn, handleSearch, search, setSearch, handleKeyDown, handleChange, handleReset }) => {
@@ -7,7 +8,10 @@ const SearchComponent = ({ showBtn, setShowBtn, handleSearch, search, setSearch,
             <>
                 {!search && <Search className="absolute top-1/2 left-3 -translate-y-1/2 text-[#AFAFAF]" />}
                 <input
-                    className={`w-full rounded-[8px] border border-solid border-[#AFAFAF] px-[${search ? '16px' : '44px'}] py-[12px] focus:outline-0`}
+                    className={cn(
+                        'w-full rounded-[8px] border border-solid border-[#AFAFAF] py-[12px] focus:outline-0',
+                        search ? 'px-[16px]' : 'px-[44px]',
+                    )}
                     type="text"
                     placeholder="Search student"
                     value={search}
