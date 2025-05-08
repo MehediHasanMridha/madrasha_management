cfc0d66a217128e8bd3078b9b0556d91c029ffdb
MehediHasanMridha <mehedimridhacse@gmail.com>
2025-05-08 06:30:41 +0600

    fix(Finance): update modal title based on route
    - Dynamically sets modal title.
    - Uses route() to determine title.
    - 'Add Money' for earnings route.
    - 'Add Voucher' for other routes.

--- resources/js/Components/Finance/ModalComponent.jsx
+++ resources/js/Components/Finance/ModalComponent.jsx
@@ -3,7 +3,11 @@ import ModalUI from '@/Components/UI/ModalUI';
 const ModalComponent = ({ modal, setModal, content }) => {
     return (
         <ModalUI
-            title={<h1 className="border-b border-[#AFAFAF] text-[20px] font-semibold text-[#111827]">Add Money</h1>}
+            title={
+                <h1 className="border-b border-[#AFAFAF] text-[20px] font-semibold text-[#111827]">
+                    {route().current('finance.earnings') ? 'Add Money' : 'Add Voucher'}
+                </h1>
+            }
             handleCancel={() => setModal(false)}
             isModalOpen={modal}
             style={{ top: 0 }}
