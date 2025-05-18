e18b36878fa2853badb148a658fa4cdb980954dd
MehediHasanMridha <mehedimridhacse@gmail.com>
2025-05-18 23:29:02 +0600

    feat(analytics): integrate Clarity.ms analytics
    - Added Clarity.ms tracking script
    - Included script in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,6 +14,13 @@
     @viteReactRefresh
     @vite(['resources/js/app.jsx', "resources/js/pages/{$page['component']}.jsx"])
     @inertiaHead
+    <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "rllay4c6ty");
+</script>
 </head>
 
 <body class="font-sans antialiased">
