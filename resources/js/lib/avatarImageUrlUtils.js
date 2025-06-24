ab4a61e9906b30f7b1c02823fd5c6d365a812b7d
MehediHasanMridha <mehedimridhacse@gmail.com>
2025-06-24 20:25:36 +0600

    refactor(avatar): update avatar image url
    - Replaced avatar image URL with a new one.
    - Added random color generation for avatars.
    - Improved avatar image fallback handling.

--- resources/js/lib/avatarImageUrlUtils.js
+++ resources/js/lib/avatarImageUrlUtils.js
@@ -1,8 +1,10 @@
 export function getAvatarImage(img = null, folderName, name = 'Mehedi Hasan') {
+    // genarate random color code
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     if (img) {
         return `/uploads/${folderName}/${img}`;
     } else if (img === null && name) {
-        return `https://avatar.iran.liara.run/username?username=${name}`;
+        return `https://ui-avatars.com/api/?background=${randomColor}&color=fff&name=${name}`;
     }
     return null;
 }
