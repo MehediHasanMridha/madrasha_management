93c2f317d0875731417378b5cc00c38b2d2f7bc4
MehediHasanMridha <mehedimridhacse@gmail.com>
2025-07-13 19:46:30 +0600

    chore(deploy): remove unused vendor folder
    - Removed commented-out vendor folder
    - Updated app name in app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,7 +4,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import '../css/app.css';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Madrasatul-Hera';
+const appName = import.meta.env.VITE_APP_NAME || 'Miftahul Quran';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
